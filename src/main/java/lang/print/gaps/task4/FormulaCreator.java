dcfc856495bfa7d9787f3c9f54792a563894e680
OfelyaSCS <sahakyanscofelya@gmail.com>
2022-09-29 20:10:52 +0400

    stage0-module3-task4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.task4;
 
+import java.util.Scanner;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a ");
+        int a= input.nextInt();
+        System.out.print("Enter b ");
+        int b= input.nextInt();
+      double formula=(9*Math.pow(a,2)-5*b+2+a-7)*((a+b-4*a*b)/2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char a='c';
+        char b='h';
+        char c='a';
+        char d='r';
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(d);
     }
 }
